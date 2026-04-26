31fc48280207f76a53a83d46bb471fd7d57abf64
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-04-26 19:43:35 +0300

    task05 update

--- Task05/logic.cpp
+++ Task05/logic.cpp
@@ -8,12 +8,16 @@
 #include "logic.h"
 
 void reverse(int array[], int size) {
-	if (size <= 0 || size > DEFAULT_SIZE) {
+	if (size <= 0) {
 		return;
 	}
-	for (int i = 0; i < (size - 1) / 2; i++) {
-		int j = size - 1;
+
+	int j = size - 1;
+
+	for (int i = 0; i < size / 2; i++) {
+		int t = array[i];
 		array[i] = array[j];
+		array[j] = t;
 		j--;
 	}
 }
